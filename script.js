58e7e2508416abf66d7f6b722ac027c0c42435e9
Ali Baghdadi <abgr8gr8@gmail.com>
2025-02-13 22:18:08 +0330

    Step 42

--- script.js
+++ script.js
@@ -38,3 +38,15 @@ function goCave() {
 function fightDragon() {
     console.log("Fighting dragon.");
 }
+
+function buyHealth() {
+
+}
+
+function buyWeapon() {
+
+}
+
+function goTown() {
+    
+}
