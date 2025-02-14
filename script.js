c1c6a4480ab8f07ba8def3ae84192da127f99bb4
Ali Baghdadi <abgr8gr8@gmail.com>
2025-02-14 14:33:58 +0330

    Step 51

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let currentWeaponIndex = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -20,7 +21,7 @@ const cat = {
     name: "Whiskers",
     "Number of legs": 4
 };
-console.log(cat);
+console.log(cat.name);
 
 // initialize buttons
 button1.onclick = goStore;
