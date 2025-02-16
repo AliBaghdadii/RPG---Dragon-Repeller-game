a299867684a1b13a9761d5454a50d7b9d28d1d56
Ali Baghdadi <abgr8gr8@gmail.com>
2025-02-16 22:31:00 +0330

    Step 123

--- script.js
+++ script.js
@@ -169,6 +169,7 @@ function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
     health -= monsters[fighting].level;
+    monsterHealth -= weapons[fighting].power;
 }
 
 function dodge() {
