d0856732951bcc5ef5c61d86990e900e35fbbabd
Ali Baghdadi <abgr8gr8@gmail.com>
2025-02-16 21:48:19 +0330

    Step 118

--- script.js
+++ script.js
@@ -160,6 +160,7 @@ function fightDragon() {
 function goFight() {
     update(locations[3]);
     monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = 'block';
 }
 
 function attack() {
