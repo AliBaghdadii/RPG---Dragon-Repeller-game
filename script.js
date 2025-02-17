9151fbed1b68b3187f413db75b0fa3b1cc02f212
Ali Baghdadi <abgr8gr8@gmail.com>
2025-02-17 17:14:50 +0330

    Step 145

--- script.js
+++ script.js
@@ -199,6 +199,10 @@ function attack() {
     }
 }
 
+function getMonsterAttackValue(level) {
+
+}
+
 function dodge() {
     text.innerText = "You dodge the attack from the " + monsters[fighting].name + ".";
 }
